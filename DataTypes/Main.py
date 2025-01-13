61784125209357661b3d86157dcb1c7ec21b7acf
isaac-miles <isaacnice247@gmail.com>
2025-01-13 17:34:38 +0100

    python work space

--- DataTypes/Main.py
+++ DataTypes/Main.py
@@ -2,3 +2,7 @@ dictionary = {"M1":"Miles",2:"Davies"}
 print(dictionary.get("M3","File not found"))
 dataset = ['miles','Aaron','United',1]
 datasetA = ('miles','Aaron','United',1)
+
+st ="This is a word"
+for w in st:
+    print(w.upper())
