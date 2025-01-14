be6f7691bc6a3ab6ad1b70cf78924c72e25aeef9
isaac-miles <isaacnice247@gmail.com>
2025-01-14 22:39:01 +0100

    python work space

--- DataTypes/Main.py
+++ DataTypes/Main.py
@@ -1,7 +1,7 @@
 dictionary = {"M1":"Miles",2:"Davies"}
 print(dictionary.get("M3","File not found"))
 dataset = ['miles','Aaron','United',1]
-datasetA = ('miles','Aaron','United',1)
+# datasetA = ('miles','Aaron','United',1)
 
 words ="This is a word"
 word = words.split(' ')
