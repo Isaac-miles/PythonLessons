547bc0485f73841d2da3e092c1a6fc2106b75b7c
isaac-miles <isaacnice247@gmail.com>
2025-01-20 17:15:29 +0100

    control statements

--- DataTypes/Tuples.py
+++ DataTypes/Tuples.py
@@ -3,3 +3,7 @@ print(len(my_tuple))
 
 this_tuple = ("apple",)
 print(type(this_tuple))
+
+the_tuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(the_tuple[2:5])
+print(the_tuple[-4:-1])
