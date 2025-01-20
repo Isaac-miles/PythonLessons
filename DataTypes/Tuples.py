444d3b83751240118c208d1485036e81f5a83d47
isaac-miles <isaacnice247@gmail.com>
2025-01-20 17:18:08 +0100

    control statements

--- DataTypes/Tuples.py
+++ DataTypes/Tuples.py
@@ -11,3 +11,11 @@ print(the_tuple[-4:-1])
 th_tuple = ("apple", "banana", "cherry")
 if "apple" in th_tuple:
   print("Yes, 'apple' is in the fruits tuple")
+
+# Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
+# But there is a workaround. You can convert the tuple into a list, change the list, and convert the list back into a tuple.
+x = ("apple", "banana", "cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+print(x)
