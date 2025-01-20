dd1edcb79bc50e4067c93a1f184a27f16c05908e
isaac-miles <isaacnice247@gmail.com>
2025-01-20 16:53:31 +0100

    control statements

---
+++ DataTypes/DataType.py
@@ -0,0 +1,8 @@
+
+the_list = ["apple", "banana", "cherry", "apple", "cherry"]
+print(the_list)
+print(len(the_list))
+
+#using list constructor
+this_list = list(("apple", "banana", "cherry","Melon")) # note the double round-brackets
+print(this_list[1:3])
