2f2ea89a4a375a41b2ea14fe97e850ecf49e4b70
isaac-miles <isaacnice247@gmail.com>
2025-01-20 16:42:42 +0100

    control statements

--- DataTypes/Loops.py
+++ DataTypes/Loops.py
@@ -23,4 +23,10 @@ for name in listNames:
 for i in range(4,16,2):
     print(i)
 
-#list comprehensions
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+#Nested loops
+for x in adj:
+  for y in fruits:
+    print(x, y)
