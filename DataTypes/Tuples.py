5d595a08ba3d62f183ff4f03b3960a5bf9fb0641
isaac-miles <isaacnice247@gmail.com>
2025-01-20 17:16:34 +0100

    control statements

--- DataTypes/Tuples.py
+++ DataTypes/Tuples.py
@@ -7,3 +7,7 @@ print(type(this_tuple))
 the_tuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
 print(the_tuple[2:5])
 print(the_tuple[-4:-1])
+
+th_tuple = ("apple", "banana", "cherry")
+if "apple" in th_tuple:
+  print("Yes, 'apple' is in the fruits tuple")
