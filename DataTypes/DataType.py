173c62c4e96640b3ae3bb34a7010fc4fcfb541bf
isaac-miles <isaacnice247@gmail.com>
2025-01-20 16:57:14 +0100

    control statements

--- DataTypes/DataType.py
+++ DataTypes/DataType.py
@@ -1,12 +1,19 @@
 
 the_list = ["apple", "banana", "cherry", "apple", "cherry"]
+#list method
+the_list.append("orange")
 print(the_list)
 print(len(the_list))
 
 #using list constructor
 this_list = list(("apple", "banana", "cherry","Melon")) # note the double round-brackets
 print(this_list[1:3])
+this_list.remove("banana")
 
 th_list = ["apple", "banana", "cherry"]
 if "apple" in th_list:
   print("Yes, 'apple' is in the fruits list")
+
+tha_list = ["apple", "banana", "cherry"]
+for x in tha_list:
+  print(x)
