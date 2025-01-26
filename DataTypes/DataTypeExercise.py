bc4325525a4e2c04bb94f27b29bf4e9d364d7b7e
isaac-miles <isaacnice247@gmail.com>
2025-01-26 15:15:25 +0100

    control statements

--- DataTypes/DataTypeExercise.py
+++ DataTypes/DataTypeExercise.py
@@ -1,3 +1,4 @@
 print("Welcome to the tip calculator")
 bill = float(input("What was the total Bill ?"))
 tip = int(input("What % tip would you like to give ? 10 12 15 "))
+people = int(input("How many people to split the bill ? "))
