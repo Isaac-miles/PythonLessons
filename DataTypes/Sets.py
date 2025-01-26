d3a63d76c19c927423ba94231bf4151b09694297
isaac-miles <isaacnice247@gmail.com>
2025-01-26 15:50:12 +0100

    control statements

--- DataTypes/Sets.py
+++ DataTypes/Sets.py
@@ -40,3 +40,10 @@ The difference() method keeps the items from the first set that are not in the o
 
 The symmetric_difference() method keeps all items EXCEPT the duplicates.
 """
+
+set1 = {"a", "b", "c"}
+set2 = {1, 2, 3}
+
+set3 = set1.union(set2)
+print(set3)
+# you can use the | operator in place of union()
