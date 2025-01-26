17d9dd6cc87f3463b095ec15d1aa1f371552f8e5
isaac-miles <isaacnice247@gmail.com>
2025-01-26 15:20:35 +0100

    control statements

--- DataTypes/DataTypeExercise.py
+++ DataTypes/DataTypeExercise.py
@@ -3,4 +3,6 @@ bill = float(input("What was the total Bill ?"))
 tip = int(input("What % tip would you like to give ? 10 12 15 "))
 people = int(input("How many people to split the bill ? "))
 bill_with_tip = tip/100 * bill + bill
-print(f"Bill with tip = {bill_with_tip}")
+bill_per_person = bill_with_tip /people
+final_Amount = round(bill_per_person, 2)
+print(f"Each person should pay: = {final_Amount}")
