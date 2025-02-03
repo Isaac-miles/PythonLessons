0fc2041ba9acb3745a00b7e5a1d189015c3ce13e
isaac-miles <isaacnice247@gmail.com>
2025-02-03 15:56:58 +0100

    data types

--- Functions/functions.py
+++ Functions/functions.py
@@ -2,3 +2,8 @@
 def myFunction(name):
     print(f"value received is {name}")
 myFunction("miles")
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
