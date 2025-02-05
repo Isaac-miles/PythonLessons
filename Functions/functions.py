a71f3b100a6d7b7bf89ae518fe657a55b70651e9
isaac-miles <isaacnice247@gmail.com>
2025-02-05 22:04:53 +0100

    data types

--- Functions/functions.py
+++ Functions/functions.py
@@ -7,3 +7,8 @@ def my_function(*kids):
   print("The youngest child is " + kids[2])
 
 my_function("Emil", "Tobias", "Linus")
+
+def my_function(*kids):
+  print("The youngest child is " + kids[2])
+
+my_function("Emil", "Tobias", "Linus")
