b5552c5437c97f645f6f6cf213ef5add4a6edacf
isaac-miles <isaacnice247@gmail.com>
2025-02-08 21:08:36 +0100

    functions

--- Functions/functions.py
+++ Functions/functions.py
@@ -40,3 +40,7 @@ my_function(x = 3)
 
 # You can combine the two argument types in the same function.
 # Any argument before the / , are positional-only, and any argument after the *, are keyword-only.
+def my_function(a, b, /, *, c, d):
+  print(a + b + c + d)
+
+my_function(5, 6, c = 7, d = 8)
