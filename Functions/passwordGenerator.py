0ffa3988a7ba0acba18aa4892b8e1ec17a1f8042
isaac-miles <isaacnice247@gmail.com>
2025-02-09 18:17:27 +0100

    classes

--- Functions/passwordGenerator.py
+++ Functions/passwordGenerator.py
@@ -9,8 +9,8 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level
-password = ""
+#simple
+# password = ""
 
 # for char in range(0, nr_letters):
 #   password += random.choice(letters)
@@ -22,3 +22,15 @@ password = ""
 #   password += random.choice(numbers)
 #
 # print(password)
+
+# complex method
+
+password = []
+for char in range(0, nr_letters):
+    password.append(char)
+
+
+
+
+
+
