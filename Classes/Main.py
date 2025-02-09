30e08d7a1aee2af1c2ac51b0077d5ecee785d184
isaac-miles <isaacnice247@gmail.com>
2025-02-09 17:30:00 +0100

    lambda functions

---
+++ Classes/Main.py
@@ -0,0 +1,7 @@
+
+"""
+All classes have a function called __init__(), which is always executed when the class is being initiated.
+
+Use the __init__() function to assign values to object properties, or other operations that are necessary to do when the object is being created:
+"""
+
