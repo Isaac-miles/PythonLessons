b3c42397682bcee0b0c24a4817a3dede4eeb2281
isaac-miles <isaacnice247@gmail.com>
2025-02-09 22:36:35 +0100

    classes

--- Functions/caeserCipher.py
+++ Functions/caeserCipher.py
@@ -7,7 +7,6 @@ chr(i): Converts the ASCII value back to its corresponding character.
 List comprehension: Creates a list of characters from 'A' to 'Z'."""
 
 alphabets = [chr(i) for i in range(ord('A'), ord('Z')+1)]
-print(alphabets[4])
 
 action = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Enter the message you want to encrypt\n")
@@ -17,5 +16,5 @@ encrypted_text =""
 def encrypt_message(original_text,shift_amount):
     for letter in original_text:
         shifted_position = alphabets.index(letter)+ shift
-        shifted_position = shifted_position % len(alphabets)
+        shifted_position %= len(alphabets)
         cipher_text = alphabets[shifted_position]
