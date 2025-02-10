a3c1e74e70e5097e0be2dc06beaad950ef12e30e
isaac-miles <isaacnice247@gmail.com>
2025-02-10 15:50:23 +0100

    classes

--- Classes/Main.py
+++ Classes/Main.py
@@ -3,6 +3,10 @@
 All classes have a function called __init__(), which is always executed when the class is being initiated.
 
 Use the __init__() function to assign values to object properties, or other operations that are necessary to do when the object is being created:
+
+The self parameter is a reference to the current instance of the class, and is used to access variables that belong to the class.
+
+It does not have to be named self, you can call it whatever you like, but it has to be the first parameter of any function in the class:
 """
 
 class Person:
