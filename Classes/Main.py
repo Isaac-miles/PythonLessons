8781166cfb8e7225623eba18c663340322aceb2c
isaac-miles <isaacnice247@gmail.com>
2025-02-10 15:54:57 +0100

    classes

--- Classes/Main.py
+++ Classes/Main.py
@@ -29,6 +29,7 @@ miles.age = 13
 # delete object properties
 del miles.age
 
+# class definitions cannot be empty, but if you for some reason have a class definition with no content, put in the pass statement to avoid getting an error.
 class Colors:
     pass
 
