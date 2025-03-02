e98d400ed6972234140614c0b427e55b5fea6c16
isaac-miles <isaacnice247@gmail.com>
2025-03-03 00:21:44 +0100

    classes and Objects

--- OOP/Main.py
+++ OOP/Main.py
@@ -1,3 +1,5 @@
+from operator import truediv
+
 
 class Student:
     def __init__(self,name,age,grade):
@@ -7,3 +9,15 @@ class Student:
 
     def get_grade(self):
         return self.grade
+
+class Course:
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = list
+
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
