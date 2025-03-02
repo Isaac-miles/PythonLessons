8714efbfe30e6a1b66071917bf3dd4a904414755
isaac-miles <isaacnice247@gmail.com>
2025-03-03 00:35:38 +0100

    classes and Objects

--- OOP/Main.py
+++ OOP/Main.py
@@ -12,7 +12,7 @@ class Course:
     def __init__(self, name, max_students):
         self.name = name
         self.max_students = max_students
-        self.students = list
+        self.students = []
 
     def add_student(self, student: Student):
         if len(self.students) < self.max_students:
@@ -30,3 +30,5 @@ s3 = Student('Emeka',18,65)
 java_course = Course('Java',2)
 java_course.add_student(s1)
 java_course.add_student(s2)
+
+print(java_course.students[0].name)
