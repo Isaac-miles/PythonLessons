412a2e6a2bf28186224cf863a122c63f9bc5dd82
isaac-miles <isaacnice247@gmail.com>
2025-03-03 17:50:47 +0100

    classes and Objects

--- OOP/Static_Methods.py
+++ OOP/Static_Methods.py
@@ -4,3 +4,11 @@ class Person:
 
     def __init__(self, name):
         self.name = name
+        Person.number_of_people +=1
+
+p1 = Person("tim")
+print(p1.number_of_people)
+
+p2 = Person("jill")
+
+print(p2.number_of_people)
