4e98563c6457a2bbe0cbe183badafc543e65937f
isaac-miles <isaacnice247@gmail.com>
2025-03-06 22:15:47 +0100

    classes and Objects

--- Functions/caeserCipher.py
+++ Functions/caeserCipher.py
@@ -12,26 +12,6 @@ action = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Enter the message you want to encrypt\n").lower()
 shift = int(input("enter the number of shift\n"))
 
-def encrypt_message(original_text,shift_amount):
-    encrypted_text = ""
-    for letter in original_text:
-        shifted_position = alphabets.index(letter) +shift_amount
-        shifted_position %= len(alphabets)
-        encrypted_text += alphabets[shifted_position]
-    print(encrypted_text)
-
-encrypt_message(original_text=text,shift_amount=shift)
-
-def decrypt_message(original_text, shift_amount):
-    encrypted_text = ""
-    for letter in original_text:
-        shifted_position = alphabets.index(letter) - shift_amount
-        shifted_position %= len(alphabets)
-        encrypted_text += alphabets[shifted_position]
-    print(encrypted_text)
-
-decrypt_message(original_text=text,shift_amount=shift)
-
 def caesar(original_text,shift_amount,encode_or_decode):
     encrypted_text = ""
     for letter in original_text:
